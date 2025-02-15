49f74ff10dc113b5b2de5407c5a26749280a761a
angelinaao <aoangelina302@gmail.com>
2025-02-14 22:07:58 -0500

    feb 14

---
+++ timeline/script.js
@@ -0,0 +1,51 @@
+let time12 = document.getElementById("twelve");
+let text12 = document.getElementById("twelve-box");
+
+
+time12.addEventListener("mouseenter", ()=>{
+    console.log("works")
+    text12.style.opacity = "100%";
+
+})
+time12.addEventListener("mouseleave", ()=>{
+    text12.style.opacity = "0%";
+})
+
+let time9 = document.getElementById("nine");
+let text9 = document.getElementById("nine-box");
+
+time9.addEventListener("mouseenter", ()=>{
+    console.log("works")
+    text9.style.opacity = "100%";
+
+})
+time9.addEventListener("mouseleave", ()=>{
+    text9.style.opacity = "0%";
+})
+
+let time3 = document.getElementById("three");
+let text3 = document.getElementById("three-box");
+
+time3.addEventListener("mouseenter", ()=>{
+    console.log("works")
+    text3.style.opacity = "100%";
+
+})
+time3.addEventListener("mouseleave", ()=>{
+    text3.style.opacity = "0%";
+})
+
+
+let time6 = document.getElementById("six");
+let text6 = document.getElementById("six-box");
+
+time6.addEventListener("mouseenter", ()=>{
+    console.log("works")
+    text6.style.opacity = "100%";
+
+})
+time6.addEventListener("mouseleave", ()=>{
+    text6.style.opacity = "0%";
+})
+
+// time12.addEventListener("mouseenter", ()=>{})
